467ba52d25c324b2dd87980ba72263319b079df0
Dario Ostuni <dario.ostuni@gmail.com>
2019-07-01 00:32:29 +0200

    Initial commit

---
+++ src/contragraph.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace cg {
+
+template <typename T = int64_t>
+class Graph {
+  protected:
+  public:
+    const static size_t NONE = size_t(-1);
+};
+
+} // namespace cg
